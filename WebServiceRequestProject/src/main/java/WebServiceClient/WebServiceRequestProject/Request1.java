78826bb593d85ca85a6c9724de1f48cc9ed7c297
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-04-25 12:15:07 +0300

    Lesson 7 create Request for testing "Testing" word

--- WebServiceRequestProject/src/main/java/WebServiceClient/WebServiceRequestProject/Request1.java
+++ WebServiceRequestProject/src/main/java/WebServiceClient/WebServiceRequestProject/Request1.java
@@ -1,11 +1,24 @@
 package WebServiceClient.WebServiceRequestProject;
 
+import com.aonaware.services.webservices.ArrayOfString;
+import com.aonaware.services.webservices.CountCheatService;
+import com.aonaware.services.webservices.CountCheatServiceSoap;
+import com.aonaware.services.webservices.LetterSolutions;
+
 public class Request1 {
 
 	public static void main(String[] args) 
 	{
+		//call the webservice
+		//using WSDL
 		
-
+		CountCheatServiceSoap webservice = new CountCheatService().getCountCheatServiceSoap();
+		
+		String anagram = "testing";
+		ArrayOfString letterSolutions = webservice.letterSolutions(anagram);	
+		
+		System.out.println(letterSolutions.getString());
+				
 	}
 
 }
