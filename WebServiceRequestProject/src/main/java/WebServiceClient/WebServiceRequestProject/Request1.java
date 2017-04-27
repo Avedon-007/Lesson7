f73590337975ba74968a91687c11f530eabf0414
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-04-27 23:25:42 +0300

    Lesson 7 add Assert check for Request1

--- WebServiceRequestProject/src/main/java/WebServiceClient/WebServiceRequestProject/Request1.java
+++ WebServiceRequestProject/src/main/java/WebServiceClient/WebServiceRequestProject/Request1.java
@@ -1,12 +1,17 @@
 package WebServiceClient.WebServiceRequestProject;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 import com.aonaware.services.webservices.ArrayOfString;
 import com.aonaware.services.webservices.CountCheatService;
 import com.aonaware.services.webservices.CountCheatServiceSoap;
 
 public class Request1 
 {
-	public static void main(String[] args) 
+	
+	public static void main(String[] args) 	
+	//public String request(String testWord)
 	{
 		//call the webservice
 		//using WSDL
@@ -14,8 +19,28 @@ public class Request1
 		CountCheatServiceSoap webservice = new CountCheatService().getCountCheatServiceSoap();
 		
 		String anagram = "testing";
+		//String anagram = testWord;
+		
 		ArrayOfString letterSolutions = webservice.letterSolutions(anagram);	
 		
-		//System.out.println(letterSolutions.getString());				
+		System.out.println(letterSolutions.getString());	
+		//String anagram1 = letterSolutions.getString();
+		//System.out.println(anagram1);
+		//return anagram1;
+		
+		
+		
+		 
+
+		StringBuilder sb = new StringBuilder();
+
+		for(String s: letterSolutions.getString()) {
+		   sb.append(s).append(", ");
+		}
+
+		sb.deleteCharAt(sb.length()-1); //delete last space
+		sb.deleteCharAt(sb.length()-1); //delete last comma
+		String newString = sb.toString();
+		System.out.println(newString);
 	}
 }
--- WebServiceRequestProject/src/test/java/WebServiceClient/WebServiceRequestProject/TestRequestForLetterSolutions.java
+++ WebServiceRequestProject/src/test/java/WebServiceClient/WebServiceRequestProject/TestRequestForLetterSolutions.java
@@ -1,6 +1,24 @@
 package WebServiceClient.WebServiceRequestProject;
 
+import static org.testng.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+import com.aonaware.services.webservices.ArrayOfString;
+
 public class TestRequestForLetterSolutions 
 {
-
+	@Test
+	public void testRequest()
+	{
+		String validString = "";
+		
+		Request1 requestInstance = new Request1();
+		
+		
+		requestInstance.request("testing");
+		assertTrue(" ", validString.equalsIgnoreCase(requestInstance.request("testing");));
+	}
 }
